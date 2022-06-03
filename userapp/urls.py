44c31718133cf30dc0ce5e2690d05eb4f4b406e2
Emmuui <nikityuk.off@gmail.com>
2022-06-03 15:43:18 +0300

    add rest_framework login, logout, registration

--- userapp/urls.py
+++ userapp/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from .views import (GetUserListView, GetUserDetailView,
-                    PutUserDetailView)
+                    PutUserDetailView, RegisterView,
+                    ObtainTokenView, LogoutView)
+from rest_framework_simplejwt.views import TokenRefreshView
+
 
 urlpatterns = [
     path('user/list/', GetUserListView.as_view()),
     path('user/get_detail/<int:pk>/', GetUserDetailView.as_view()),
     path('user/put_detail/<int:pk>/', PutUserDetailView.as_view()),
+    path('user/register/', RegisterView.as_view()),
+    path('user/login/', ObtainTokenView.as_view(), name='token_obtain_pair'),
+    path('user/login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('user/logout/', LogoutView.as_view(), name='auth_logout'),
 ]
--- userapp/views.py
+++ userapp/views.py
@@ -2,10 +2,46 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAdminUser
-from .serializers import UserListSerializer
+from rest_framework.permissions import IsAdminUser, AllowAny, IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import (UserListSerializer, RegisterSerializer,
+                          TokenObtainSerializer)
 from .models import UserProfile
 from .permissions import IsOwnerOnly
+
+
+class LogoutView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class ObtainTokenView(TokenObtainPairView):
+    """ Generate pair of token refresh and token access """
+
+    permission_classes = (AllowAny,)
+    serializer_class = TokenObtainSerializer
+
+
+class RegisterView(APIView):
+    """ Registration view for all user """
+    permission_classes = (AllowAny, )
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class PutUserDetailView(APIView):
